0b354729a00231ba175d285861ec97cd5d820c8a
Aaron <aaron.stadelmann@regisroyals.org>
2020-06-23 12:17:40 -0500

    Completed mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -2,26 +2,45 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@Autonomous(name = "Tank Drive")
+@TeleOp(name = "Tank Drive")
 public class TankDrive extends OpMode {
 
-    DcMotor l_drive;
-    DcMotor r_drive;
+    DcMotor lf_drive;
+    DcMotor lb_drive;
+    DcMotor rf_drive;
+    DcMotor rb_drive;
 
     @Override
     public void init() {
-        l_drive = hardwareMap.get(DcMotor.class, "l_drive");
-        r_drive = hardwareMap.get(DcMotor.class, "r_drive");
+        lf_drive = hardwareMap.get(DcMotor.class, "lf_drive");
+        lb_drive = hardwareMap.get(DcMotor.class, "lb_drive");
+        rf_drive = hardwareMap.get(DcMotor.class, "rf_drive");
+        rb_drive = hardwareMap.get(DcMotor.class, "rb_drive");
 
-        l_drive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rf_drive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rb_drive.setDirection(DcMotorSimple.Direction.REVERSE);
     }
+
 
     @Override
     public void loop() {
-        l_drive.setPower(1);
-        r_drive.setPower(1);
+
+        double lfp = gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x;
+        double lbp = gamepad1.left_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x;
+        double rfp = gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x;
+        double rbp = gamepad1.left_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x;
+
+
+
+        lf_drive.setPower(lfp);
+        lb_drive.setPower(lbp);
+        rf_drive.setPower(rfp);
+        rb_drive.setPower(rbp);
+
+
     }
 }
